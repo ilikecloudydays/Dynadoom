334e3d08bfc6f06c596434298b7646c059473862
Gamorr <formelakuba7@gmail.com>
2022-11-14 15:51:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Dynadoom
+
+Developed with Unreal Engine 5
---
+++ Source/Dynadoom/DynadoomGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DynadoomGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DYNADOOM_API ADynadoomGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
